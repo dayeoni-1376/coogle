3c5e69cfee78e8540f3f58f80756eba2af313825
dayeoni-1376 <udy1376@gmail.com>
2020-07-04 15:15:02 +0900

    Set S3

--- coogle/settings/prod.py
+++ coogle/settings/prod.py
@@ -13,3 +13,14 @@ ALLOWED_HOSTS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# S3
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'coogle'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+AWS_S3_ADDRESSING_STYLE = 'virtual' # auto, 'path', 'virtual'
+AWS_DEFAULT_ACL = None
